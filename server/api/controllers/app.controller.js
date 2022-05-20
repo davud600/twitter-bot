33b6d4188744fdde50c352d67f9e544fb083bf5c
davud600 <davudgorqi@gmail.com>
2022-05-20 14:14:46 +0300

    stuff

--- server/api/controllers/app.controller.js
+++ server/api/controllers/app.controller.js
@@ -1,10 +1,42 @@
+import fetch from "node-fetch"
+
+const token = process.env.BEARER_TOKEN
+// const api = "https://api.twitter.com/2"
+const api = "http://localhost/5000"
+
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${token || ""}`
+  }
+}
+
 export default class AppController {
   static async apiGetDescriptionText(req, res) {
-    // Get users tweets from body
-    const { tweets } = req.body
+    // Get users username from body
+    const { username } = req.body
+
+    let tweets_of_user
+    // Get tweets of user
+    try {
+      const user_req = await fetch(
+        `${api}/users/by/username/${username}`,
+        options
+      )
+      const { data: user } = await user_req.json()
+      
+      const tweets_of_user_req = await fetch(
+        `${api}/users/${user.id}/tweets`,
+        options
+      )
+      const { data: tweets_of_user } = await tweets_of_user_req.json()
+    } catch (e) {
+      console.error(e)
+    }
 
     // Generate description
-
+    
 
     // Send back description
     const user_description = "ye bro u good"
--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 import app from "./server.js"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const port = 5000
 
